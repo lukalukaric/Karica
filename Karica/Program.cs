7453d6661de7e0a4e954dcb9d11fb6b8b0370800
Luka <Luka@LUKA-PC>
2021-07-15 12:18:56 +0200

    Add project files.

---
+++ Karica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Karica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
