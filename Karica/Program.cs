7946a69d6b439bee33d5ac1671f094c6a40421e1
Luka Lukarič <luka.lukaric@student.um.si>
2021-07-15 12:48:50 +0200

    obroba kare

--- Karica/Program.cs
+++ Karica/Program.cs
@@ -6,6 +6,7 @@ namespace Karica
     {
         static void Main(string[] args)
         {
+            // faza 1
             int n;
             Console.WriteLine("Vnesi višino lika");
             n = Convert.ToInt32(Console.ReadLine());
@@ -34,6 +35,53 @@ namespace Karica
                 for (int j = 0; j < i * 2 + 1; j++)
                 {
                     Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+
+            // faza 2 obroba kare
+            Console.WriteLine("");
+
+            breaks = n / 2;
+            for (int i = 0; i <= n / 2; i++)
+            {
+                for (int k = 0; k < breaks; k++)
+                {
+                    Console.Write(" ");
+                }
+                breaks--;
+                for (int j = 0; j < i * 2 + 1; j++)
+                {
+                    if(j == 0 || j == i * 2)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    
+                }
+                Console.WriteLine("");
+            }
+            breaks = 1;
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                for (int k = 0; k < breaks; k++)
+                {
+                    Console.Write(" ");
+                }
+                breaks++;
+                for (int j = 0; j < i * 2 + 1; j++)
+                {
+                    if (j == 0 || j == i * 2)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
                 }
                 Console.WriteLine("");
             }
