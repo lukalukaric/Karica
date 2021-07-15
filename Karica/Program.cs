5c58028c887b4d694d1c8c6a7b85c8f6e8d0e2ff
Luka Lukarič <luka.lukaric@student.um.si>
2021-07-15 12:36:37 +0200

    Faza 1

--- Karica/Program.cs
+++ Karica/Program.cs
@@ -6,7 +6,37 @@ namespace Karica
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n;
+            Console.WriteLine("Vnesi višino lika");
+            n = Convert.ToInt32(Console.ReadLine());
+            int breaks = n / 2;
+            for(int i = 0; i <= n/2; i++)
+            {
+                for(int k = 0; k < breaks; k++)
+                {
+                    Console.Write(" ");
+                }
+                breaks--;
+                for(int j = 0; j < i*2+1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            breaks = 1;
+            for(int i = n/2 - 1; i >= 0; i--)
+            {
+                for (int k = 0; k < breaks; k++)
+                {
+                    Console.Write(" ");
+                }
+                breaks++;
+                for (int j = 0; j < i * 2 + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
         }
     }
 }
